86df2dfcc726c2f57ead17df54f3ef5d38dd4813
Connor Savage <csavage@mdsol.com>
2014-08-05 19:13:34 -0400

    remove ambivalent */*

--- lib/rack/accept_default.rb
+++ lib/rack/accept_default.rb
@@ -6,7 +6,9 @@ module Rack
     end
 
     def call(env)
+      env['HTTP_ACCEPT'] = nil if env['HTTP_ACCEPT'] == "*/*"
       env['HTTP_ACCEPT'] ||= @default
+
       @app.call(env)
     end
   end
