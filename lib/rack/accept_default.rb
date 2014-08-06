30ee93b9d21f7295bc288c4ee768b6690c662d07
Connor Savage <csavage@mdsol.com>
2014-08-06 17:11:46 -0400

    add eureka client specific override, update spec

--- lib/rack/accept_default.rb
+++ lib/rack/accept_default.rb
@@ -6,6 +6,7 @@ module Rack
     end
 
     def call(env)
+      env['HTTP_ACCEPT'] = nil if env['HTTP_ACCEPT'] == "*/*" && env['HTTP_USER_AGENT'] =~ /google-api-ruby-client/
       env['HTTP_ACCEPT'] ||= @default
       @app.call(env)
     end
--- spec/rack/accept_default_spec.rb
+++ spec/rack/accept_default_spec.rb
@@ -3,26 +3,42 @@ require 'spec_helper'
 describe Rack::AcceptDefault do
   include Rack::Test::Methods
 
-  def app
-    rack_app = lambda { |env| [200, {"Content-Type" => "text/plain"}, [ env["HTTP_ACCEPT"] ]] }
-    Rack::AcceptDefault.new(rack_app)
+  context 'given no argument for default' do
+    def app
+      rack_app = lambda { |env| [200, {"Content-Type" => "text/plain"}, [ env["HTTP_ACCEPT"] ]] }
+      Rack::AcceptDefault.new(rack_app)
+    end
+
+    it "should set */* as a default header" do
+      get '/'
+      last_response.should be_ok
+      last_response.body.should == '*/*'
+    end
+
+    it "should override given nil Accept header" do
+      get "/", {}, { "HTTP_ACCEPT" => nil }
+      last_response.should be_ok
+      last_response.body.should == "*/*"
+    end
+
+    it "should not override given Accept header" do
+      get '/', {}, { 'HTTP_ACCEPT' => "application/json" }
+      last_response.should be_ok
+      last_response.body.should == 'application/json'
+    end
   end
 
-  it "should set */* as a default header" do
-    get '/'
-    last_response.should be_ok
-    last_response.body.should == '*/*'
+  context 'given an argument for default' do
+    def app
+      rack_app = lambda { |env| [200, {"Content-Type" => "text/plain"}, [ env["HTTP_ACCEPT"] ]] }
+      Rack::AcceptDefault.new(rack_app, "application/json")
+    end
+
+    it "should override the */* Accept header with application/json if the User Agent is google-api-ruby-client" do
+      get '/', {}, { 'HTTP_ACCEPT' => "*/*", 'HTTP_USER_AGENT' => "google-api-ruby-client/0.4.0 Mac OS X/10.9.4" }
+      last_response.should be_ok
+      last_response.body.should == 'application/json'
+    end
   end
 
-  it "should override given nil Accept header" do
-    get "/", {}, { "HTTP_ACCEPT" => nil }
-    last_response.should be_ok
-    last_response.body.should == "*/*"
-  end
-
-  it "should not override given Accept header" do
-    get '/', {}, { 'HTTP_ACCEPT' => "application/json" }
-    last_response.should be_ok
-    last_response.body.should == 'application/json'
-  end
 end
